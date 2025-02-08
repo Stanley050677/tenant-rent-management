f99c7d185aecf6a6a4bb76decd9cf792ee314da0
Stanlee <staszekjanusz30@gmail.com>
2025-02-08 03:30:31 +0100

    Describe recent changes or progress

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "tenantrentmanagement1b566294": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
