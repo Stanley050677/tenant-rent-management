45d386d6d986e968ff6690b12a403d482579ba3f
Stanlee <staszekjanusz30@gmail.com>
2025-02-09 14:15:24 +0100

    Update App.js: use named import for Amplify from aws-amplify

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
 import React from 'react';
-import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import AdminDashboard from './components/Admin/AdminDashboard';
 import TenantDashboard from './components/Tenant/TenantDashboard';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        {/* Navigation Bar */}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/admin">Admin Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/tenant">Tenant Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* Route definitions */}
-        <Routes>
-          <Route path="/" element={<h1>Welcome to Tenant Rent Management</h1>} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/tenant" element={<TenantDashboard />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AmplifyAuthenticator>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/admin">Admin Dashboard</Link></li>
+              <li><Link to="/tenant">Tenant Dashboard</Link></li>
+            </ul>
+          </nav>
+          <AmplifySignOut />
+          <Routes>
+            <Route path="/" element={<h1>Welcome to Tenant Rent Management</h1>} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/tenant" element={<TenantDashboard />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AmplifyAuthenticator>
   );
 }
 
 export default App;
-
-
 
