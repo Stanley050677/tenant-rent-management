5b3625860b6f6f8160c603291d96f5e97ade1dc0
Stanlee <staszekjanusz30@gmail.com>
2025-02-09 03:14:48 +0100

    Fix case-sensitive path for TenantDashboard component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 // Find the root element in your HTML
 const container = document.getElementById('root');
